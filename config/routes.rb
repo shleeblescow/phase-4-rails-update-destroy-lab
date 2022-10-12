ed909c252fd8c3e98d1867d6af62120c2b54d388
Shelby Lee <slee337@uw.edu>
2022-10-12 11:24:48 -0600

    commit tree 92c14f0aeb91a84a3770d2e3cb1262d6f684c952

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants, only: [:index, :show, :create, :update, :destroy]
+  # patch "/birds/:id/like", to: "birds#increment_likes"
 end
